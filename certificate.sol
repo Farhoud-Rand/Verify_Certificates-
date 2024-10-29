8f082ac666d0090a2952cdd9559166cdd5e2e911
Rand <r.farhoud2000@gmail.com>
2024-10-29 14:06:13 +0200

    Certificate code version 4 - gas optimization

--- certificate.sol
+++ certificate.sol
@@ -3,76 +3,74 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract certificateNFT is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Ownable, AccessControl {
+contract certificateNFT is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, AccessControl, ReentrancyGuard {
     // Define roles
     bytes32 public constant ISSUER_ROLE = keccak256("ISSUER_ROLE");
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
     // Variable to make the contract auto Increment Ids for tokens
     uint256 private _nextTokenId;
-    
     //-----------------------------------------------------------------
     // Constructor
     //-----------------------------------------------------------------
-    constructor() ERC721("Rand Collection", "RC") Ownable(msg.sender) {
+    constructor() ReentrancyGuard() ERC721("Rand Collection", "RC") {
         // Give the contract depolyer admin role
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(ADMIN_ROLE, msg.sender);
     }
-
     //-----------------------------------------------------------------
     // Admin Functionality
     //-----------------------------------------------------------------
-    function assignRole(string memory role, address account) public onlyRole(ADMIN_ROLE) {
-        bytes32 enteredRole = keccak256(abi.encodePacked(role));
-        if(enteredRole != ADMIN_ROLE && enteredRole != ISSUER_ROLE) {
+    function assignRole(bytes32 role, address account) public onlyRole(ADMIN_ROLE) {
+        if(role != ADMIN_ROLE && role != ISSUER_ROLE) {
             revert("Error: Invalid role");
         } else {
-            _grantRole(enteredRole, account);
+            _grantRole(role, account);
         }
     }
 
-    function revokeRoleFromUser(string memory role, address account) public onlyRole(ADMIN_ROLE) {
-        bytes32 enteredRole = keccak256(abi.encodePacked(role));
-        if(enteredRole != ADMIN_ROLE && enteredRole != ISSUER_ROLE) {
+    function revokeRoleFromUser(bytes32 role, address account) public onlyRole(ADMIN_ROLE) {
+        if(role != ADMIN_ROLE && role != ISSUER_ROLE) {
             revert("Error: Invalid role");
         } else {
-            _revokeRole(enteredRole, account);
+            _revokeRole(role, account);
         }
     }
-
     //-----------------------------------------------------------------
     // Issuer Functionality
     //-----------------------------------------------------------------
     // Minting tokens
-    function safeMint(address _to, string calldata _uri) external onlyRole(ISSUER_ROLE) {
+    function safeMint(address _to, string calldata _uri) external onlyRole(ISSUER_ROLE) nonReentrant(){
         uint256 tokenId = _nextTokenId++;
         _safeMint(_to, tokenId);
         _setTokenURI(tokenId, _uri);
     }
 
     // Burning tokens 
-    function burnToken(uint256 tokenId) public onlyRole(ISSUER_ROLE) {
+    function burnToken(uint256 tokenId) public onlyRole(ISSUER_ROLE) nonReentrant() {
         _burn(tokenId);
     }
 
     //-----------------------------------------------------------------
     // Viewer and beneficiary Functionality
     //-----------------------------------------------------------------
+    function unsafe_inc(uint x) private pure returns(uint) {
+        unchecked { return x+1; }
+    }
+
     function getTokens(address account) public view returns (uint256[] memory) {
         uint256 tokenCount = balanceOf(account);
         uint256[] memory ownedTokens = new uint256[](tokenCount);
 
-        for (uint256 i = 0; i < tokenCount; i++) {
+        for (uint256 i = 0; i < tokenCount; i = unsafe_inc(i)) {
             ownedTokens[i] = tokenOfOwnerByIndex(account, i);
         }
         return ownedTokens;
     }
-    
     //-----------------------------------------------------------------
     // The following functions are overrides required by Solidity
     // Function that retrieves the metadata URI associated with a given tokenId
